8b97b58a8ce724560f64671df3615a586684f668
Serhii <schenyavsky@gmail.com>
2023-02-12 11:58:34 +0100

    up

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,7 +12,7 @@ galleryEl.insertAdjacentHTML("beforeend", ImagesMarkup);
 function createImagesMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `<a class="galleri__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}"/>
 </a>`;
     })
