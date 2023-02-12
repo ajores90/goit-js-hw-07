bb586e735fc7721b320aee7c1bd91d6bd563d025
Serhii <schenyavsky@gmail.com>
2023-02-12 10:52:30 +0100

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const ImagesMarkup = createImagesMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", ImagesMarkup);
+
+function createImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+    })
+    .join("");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
